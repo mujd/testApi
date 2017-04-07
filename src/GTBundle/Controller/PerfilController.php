2ccfab2dc2a25a558e60c5159f713a2abd97a9ad
mujd <mujd19@gmail.com>
2017-04-07 18:53:10 -0300

    Commit masivo

--- src/GTBundle/Controller/PerfilController.php
+++ src/GTBundle/Controller/PerfilController.php
@@ -11,29 +11,49 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use GuzzleHttp\Client;
 
 class PerfilController extends Controller {
-
+    
+    
     public function listAction(Request $request) {
-        $client = new \GuzzleHttp\Client();
+        $client = new Client();
         $r = $client->request('GET', 'http://138.197.7.205/gt/api/web/perfil');
         $data = json_decode($r->getBody()->getContents(), true);
         return $this->render('GTBundle:Perfil:list.html.twig', array('perfiles' => $data));
     }
-
-       
-
-    public function createAction(Request $request) {
-        $client = new \GuzzleHttp\Client();
-        $r = $client->request('POST', 'http://138.197.7.205/gt/api/web/perfil');
-        $form = $this->createFormBuilder()
+    
+    
+    public function addAction() {
+        $perfil = new Perfil();
+        $form = $this->createFormBuilder($perfil)
                 ->add('nombre', TextType::class)
                 ->add('objetivo', TextType::class)
                 ->add('reporta', TextType::class)
                 ->add('tareas', TextType::class)
                 ->add('save', SubmitType::class, array('label' => 'Registrar'))
                 ->getForm();
-
         return $this->render('GTBundle:Perfil:add.html.twig', array(
-                    'form' => $form->createView(),
-        ));
+                    'form' => $form->createView()));
     }
+
+    public function createAction(Request $request) {
+        $perfil = new Perfil();
+        $form = $this->createFormBuilder($perfil);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            
+            $perfil = new Perfil();
+            $perfil->setNombre($form->get("nombre")->getData());
+            $perfil->setObjetivo($form->get("objetivo")->getData());
+            $perfil->setReporta($form->get("objetivo")->getData());
+            $perfil->setTareas($form->get("objetivo")->getData());
+            $client = new Client();
+            $r = $client->request('POST', 'http://138.197.7.205/gt/api/web/perfil/crear', ['body' => $perfil]);
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($r);
+//            $em->flush();
+//            $this->addFlash('mensaje', 'El Perfil fue creado con exito.');    
+            return $this->redirectToRoute('list_perfil');
+        }
+        return $this->render('GTBundle:Perfil:add.html.twig', array('form' => $form->createView()));
+    }
+
 }
